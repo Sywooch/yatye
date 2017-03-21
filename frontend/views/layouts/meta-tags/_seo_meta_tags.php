91b442f57bbada0c06e38dbbdde98cbd8b9f0cf5
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-03-22 00:14:13 +0200

    Quick fixes

--- frontend/views/layouts/meta-tags/_seo_meta_tags.php
+++ frontend/views/layouts/meta-tags/_seo_meta_tags.php
@@ -23,8 +23,8 @@ $keywords = $data['get_keywords'];
 <meta http-equiv="expires" content="30"/>
 <meta name="google-site-verification" content="PSEIUTyoAIopuk7Q8t8B6M6WiF711Udi3dT1YE6LBFk"/>
 
-<!--<meta name="serp-rank" position="1"/>-->
-<!--<meta name="serps" content="1, 2, 3, 10, 11, ATF"/>-->
-<!--<meta name="alexa" content="100"/>-->
-<!--<meta name="googlebot" content="all, index, follow, none, noindex, nofollow, noarchive, nosnippet"/>-->
-<!--<meta name="pagerank™" content="10"/>-->
+<meta name="serp-rank" position="1"/>
+<meta name="serps" content="1, 2, 3, 10, 11, ATF"/>
+<meta name="alexa" content="100"/>
+<meta name="googlebot" content="all, index, follow, none, noindex, nofollow, noarchive, nosnippet"/>
+<meta name="pagerank™" content="10"/>
