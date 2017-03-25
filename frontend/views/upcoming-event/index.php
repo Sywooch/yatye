85b458ba713633f7f3ff93149acae8e87a7220f6
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-03-25 20:01:08 +0200

    Quick fixes

--- frontend/views/upcoming-event/index.php
+++ frontend/views/upcoming-event/index.php
@@ -39,57 +39,10 @@ $this->title = $model->name;
                     <div class="col-md-6 col-lg-6 p30">
                         <div class="row">
                             <?php if (!empty($contacts)): ?>
-                                <div class="detail-contact">
-                                    <?php foreach ($contacts as $contact): ?>
-                                        <?php if ($contact->type === Yii::$app->params['PHYSICAL_ADDRESS']): ?>
-                                            <div class="detail-contact-address">
-                                                <i class="fa fa-location-arrow"></i> <a
-                                                        href="#"><?php echo $contact->name ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                        <?php if ($contact->type === Yii::$app->params['PO_BOX']): ?>
-                                            <div class="detail-contact-phone">
-                                                <i class="fa fa-at"></i> <a
-                                                        href="#"><?php echo $contact->name ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                        <?php if ($contact->type === Yii::$app->params['MOB_PHONE']): ?>
-                                            <div class="detail-contact-phone">
-                                                <i class="fa fa-mobile-phone"></i> <a
-                                                        href="#"><?php echo $contact->name ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                        <?php if ($contact->type === Yii::$app->params['LAND_LINE']): ?>
-                                            <div class="detail-contact-phone">
-                                                <i class="fa fa-phone"></i> <a
-                                                        href="#"><?php echo $contact->name ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                        <?php if ($contact->type === Yii::$app->params['FAX']): ?>
-                                            <div class="detail-contact-phone">
-                                                <i class="fa fa-fax"></i> <a
-                                                        href="#"><?php echo $contact->name ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                        <?php if ($contact->type === Yii::$app->params['EMAIL']): ?>
-                                            <div class="detail-contact-email">
-                                                <i class="fa fa-envelope-o"></i> <a
-                                                        href="mailto:#"><?php echo $contact->name ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                        <?php if ($contact->type === Yii::$app->params['SKYPE']): ?>
-                                            <div class="detail-contact-skype">
-                                                <i class="fa fa-skype"></i> <a href="<?php echo $contact->name ?>"><?php echo $contact->name ?></a>
-                                            </div>
-                                        <?php endif; ?>
-                                        <?php if ($contact->type === Yii::$app->params['WEBSITE']): ?>
-                                            <div class="detail-contact-website">
-                                                <i class="fa fa-globe"></i> <a href="<?php echo $contact->name ?>" target="_blank">Visit website</a>
-                                            </div>
-                                        <?php endif; ?>
-
-                                    <?php endforeach; ?>
-                                </div>
+                                <?php echo $this->render('_contact', [
+                                    'model' => $model,
+                                    'contacts' => $contacts,
+                                ]); ?>
                             <?php endif; ?>
                         </div>
                         <br>
@@ -97,42 +50,10 @@ $this->title = $model->name;
                         <br>
                         <div class="row">
                             <?php if (!empty($socials)): ?>
-                                <div class="detail-follow">
-                                    <?php foreach ($socials as $social):
-                                        if ($social->type == Yii::$app->params['FACEBOOK']): ?>
-                                            <a class="follow-btn facebook" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-facebook"></i></a>
-                                        <?php endif;
-                                        if ($social->type == Yii::$app->params['YOUTUBE']): ?>
-                                            <a class="follow-btn youtube" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-youtube"></i></a>
-                                        <?php endif;
-                                        if ($social->type == Yii::$app->params['TWITTER']): ?>
-                                            <a class="follow-btn twitter" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-twitter"></i></a>
-                                        <?php endif;
-                                        if ($social->type == Yii::$app->params['GOOGLE_PLUS']): ?>
-                                            <a class="follow-btn google-plus" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-google-plus"></i></a>
-                                        <?php endif;
-                                        if ($social->type == Yii::$app->params['INSTAGRAM']): ?>
-                                            <a class="follow-btn instagram" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-instagram"></i></a>
-                                        <?php endif;
-                                        if ($social->type == Yii::$app->params['PINTREST']): ?>
-                                            <a class="follow-btn pinterest" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-pinterest"></i></a>
-                                        <?php endif;
-                                        if ($social->type == Yii::$app->params['FLICKLR']): ?>
-                                            <a class="follow-btn flickr" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-flickr"></i></a>
-                                        <?php endif;
-                                        if ($social->type == Yii::$app->params['TRIPADVISOR']): ?>
-                                            <a class="follow-btn tripadvisor" href="<?php echo $social->name; ?>"
-                                               target="_blank"><i class="fa fa-tripadvisor"></i></a>
-                                        <?php endif;
-                                    endforeach; ?>
-                                </div>
+                                <?php echo $this->render('_social', [
+                                    'model' => $model,
+                                    'socials' => $socials,
+                                ]); ?>
                             <?php endif; ?>
                         </div>
                     </div>
