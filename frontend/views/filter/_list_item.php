a6676b1dbbb1d1ed2efc7d5593ab16da0a3878c6
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-03-26 22:23:20 +0200

    Quick fixes

--- frontend/views/filter/_list_item.php
+++ frontend/views/filter/_list_item.php
@@ -12,10 +12,14 @@ use yii\helpers\Url;
 <div class="card-row div item" data-key="<?php echo $model['id'] ?>">
     <div class="card-row-inner">
         <a href="<?php echo Yii::$app->request->baseUrl . '/place-details/' . $model['slug'] ?>">
-            <div class="card-row-image" data-background-image="<?php echo ($model['logo'] != null) ? Yii::$app->params['galleries'] . $model->logo : Yii::$app->params['pragmaticmates-logo-jpg'] ?>"></div>
+            <div class="card-row-image" data-background-image="<?php echo $model->getThumbnailLogo() ?>"></div>
         </a>
         <div class="card-row-body">
-            <h2 class="card-row-title"><a target="_blank" href="<?php echo Yii::$app->request->baseUrl . '/place-details/' . $model['slug'] ?>"><?php echo $model['name'] ?></a></h2>
+            <h2 class="card-row-title">
+                <a target="_blank" href="<?php echo Yii::$app->request->baseUrl . '/place-details/' . $model['slug'] ?>">
+                    <?php echo $model['name'] ?>
+                </a>
+            </h2>
             <div class="card-row-content"></div>
         </div>
         <div class="card-row-properties" style="width: 360px;">
