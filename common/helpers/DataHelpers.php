a004b7eaf24c89b1b1077738b8b5733c18ae3f18
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-03-27 06:56:39 +0200

    Quick fixes

--- common/helpers/DataHelpers.php
+++ common/helpers/DataHelpers.php
@@ -163,7 +163,7 @@ class DataHelpers
     public static function getUpcomingEvents()
     {
         return Event::find()
-            ->where(new Expression('`start_date` >= CURRENT_TIMESTAMP'))
+            ->where(new Expression('`end_date` >= CURRENT_DATE'))
             ->andWhere(['status' => Yii::$app->params['active']])
             ->orderBy(new Expression('`start_date`'))
             ->limit(8)
--- frontend/views/post-details/index.php
+++ frontend/views/post-details/index.php
@@ -2,8 +2,10 @@
 /* @var $this yii\web\View */
 use yii\helpers\Html;
 use yii\helpers\Url;
+
 $this->title = $model->title;
 ?>
+<div class="mt50">
     <div class="col-sm-8 col-lg-9">
         <div class="content">
             <div class="page-title">
@@ -12,16 +14,24 @@ $this->title = $model->title;
 
             <div class="posts post-detail">
 
-                <?php if($model->image != ''): ?>
-                    <img src="<?php echo Yii::$app->params['post_images'] . $model->image; ?>" alt="<?php echo $model->title;?>" style=' width: 100%; "Helvetica Neue", Helvetica, Arial, sans-serif; color: #5d4942; font-size: 32px;'>
+                <?php if ($model->image != ''): ?>
+                    <img src="<?php echo Yii::$app->params['post_images'] . $model->image; ?>"
+                         alt="<?php echo $model->title; ?>"
+                         style=' width: 100%; "Helvetica Neue", Helvetica, Arial, sans-serif; color: #5d4942; font-size: 32px;'>
                 <?php endif; ?>
 
 
                 <div class="post-meta clearfix div">
-                    <div class="post-meta-date"><i class="fa fa-clock-o"></i><?php echo date('D d M, Y',strtotime($model->getUpdatedAt()));?></div>
-                    <div class="post-meta-categories"><i class="fa fa-tags"></i> <a href="<?php echo Yii::$app->request->baseUrl . '/post-category/' . $post_category->slug; ?>"><?php echo $post_category->name;?></a></div>
+                    <div class="post-meta-date"><i
+                                class="fa fa-clock-o"></i><?php echo date('D d M, Y', strtotime($model->getUpdatedAt())); ?>
+                    </div>
+                    <div class="post-meta-categories"><i class="fa fa-tags"></i> <a
+                                href="<?php echo Yii::$app->request->baseUrl . '/post-category/' . $post_category->slug; ?>"><?php echo $post_category->name; ?></a>
+                    </div>
                     <div class="post-meta-categories">
-                        <div class="fb-share-button" data-href="http://rwandaguide.info/<?php echo  Yii::$app->request->getUrl() ?>" data-layout="button_count"></div>
+                        <div class="fb-share-button"
+                             data-href="http://rwandaguide.info/<?php echo Yii::$app->request->getUrl() ?>"
+                             data-layout="button_count"></div>
                     </div>
                     <div class="post-meta-categories">
                         <a class="twitter-share-button" href="https://twitter.com/intent/tweet">Tweet</a>
@@ -31,7 +41,7 @@ $this->title = $model->title;
                     </div>
                     <div class="post-meta-categories">
                         <a href="https://www.pinterest.com/pin/create/button/">
-                            <img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png" />
+                            <img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png"/>
                         </a>
                     </div>
                 </div>
@@ -45,4 +55,5 @@ $this->title = $model->title;
 
         </div>
     </div>
-<?php echo $this->render('@app/views/layouts/_right_side') ?>
+    <?php echo $this->render('@app/views/layouts/_right_side') ?>
+</div>
