2431fcb8ab63b6b49922e84293825138cc3dee5b
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-04-05 20:42:41 +0200

    Quick fixes

--- backend/views/post-category/update.php
+++ backend/views/post-category/update.php
@@ -8,9 +8,9 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => 'Post Category',
 ]) . $model->name;
-//$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Post Categories'), 'url' => ['index']];
-//$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-//$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Post Categories'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 <div class="row">
     <div class="col-sm-12">
