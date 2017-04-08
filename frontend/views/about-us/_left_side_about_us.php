f702df5d7b398e79441ec9cf7cd9f07ca07539ed
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-04-08 17:48:27 +0200

    Quick fixes

--- frontend/views/about-us/_left_side_about_us.php
+++ frontend/views/about-us/_left_side_about_us.php
@@ -12,6 +12,7 @@ use yii\helpers\Url;
 <div class="col-sm-4 col-lg-3">
     <div class="sidebar mt50">
         <div class="widget">
+            <h2 class="widgettitle"><?php echo Yii::t('app', 'About us') ?></h2>
             <?php if (!empty($about_us_posts)):?>
                 <ul class="menu">
                     <?php foreach ($about_us_posts as $about_us_post): ?>
