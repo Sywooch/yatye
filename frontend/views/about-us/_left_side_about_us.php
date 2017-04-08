efc83086a754e85cc58a915f5b62b52027171f12
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-04-08 17:50:55 +0200

    Quick fixes

--- frontend/views/about-us/_left_side_about_us.php
+++ frontend/views/about-us/_left_side_about_us.php
@@ -10,7 +10,7 @@ use yii\helpers\Url;
 ?>
 
 <div class="col-sm-4 col-lg-3">
-    <div class="sidebar mt50" style="margin-top: -15px;">
+    <div class="sidebar mt50" style="margin-top: -5px;">
         <div class="widget">
             <h2 class="widgettitle"><?php echo Yii::t('app', 'About us') ?></h2>
             <?php if (!empty($about_us_posts)):?>
