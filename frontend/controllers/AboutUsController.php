3368c0c5ed41400e5ae378072773ae736439a115
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-04-08 17:42:48 +0200

    Quick fixes

--- frontend/controllers/AboutUsController.php
+++ frontend/controllers/AboutUsController.php
@@ -12,7 +12,29 @@ class AboutUsController extends BaseController
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect(Yii::$app->params['root']);
+    }
+
+    public function actionSlug($slug)
+    {
+        $model = AboutUs::findOne(['slug' => $slug, 'status' => Yii::$app->params['active']]);
+        $about_us_posts = AboutUs::getAboutUsPosts();
+
+        if (!is_null($model)) {
+            $title = $model->title;
+            $description = $model->title;
+            $image = Yii::$app->params['post_images'] . $model->image;
+            $url = 'http://rwandaguide.info/about-us/' . $model->slug;
+            MetaTagHelpers::registerMetaTag($title, $description, $image, $url);
+
+            return $this->render('index', [
+                'model' => $model,
+                'about_us_posts' => $about_us_posts,
+            ]);
+
+        } else {
+            return $this->redirect(Yii::$app->params['root']);
+        }
     }
 
 }
