65cf612a284ff8294e03a198e82a2a71ccd2b0ff
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-04-21 17:59:04 +0200

    Quick fixes

--- api/config/main.php
+++ api/config/main.php
@@ -8,17 +8,25 @@ $params = array_merge(
 
 return [
     'id' => 'app-api',
+    'name'=>'Rwanda Guide API',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'api\controllers',
+    'modules' => require(__DIR__ . '/modules.php'),
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-api',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'user' => [
-            'identityClass' => 'common\models\User',
+            /*'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-api', 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity-api', 'httpOnly' => true],*/
+
+            'identityClass' => 'api\modules\v1\models\User',
+            'enableAutoLogin' => false,
         ],
         'session' => [
             // this is the name of the session cookie used for login on the api
@@ -37,12 +45,14 @@ return [
             'errorAction' => 'site/error',
         ],
 
-        'urlManager' => [
+        /*'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
-        ],
+        ],*/
+
+        'urlManager' => require(__DIR__ . '/routes.php'),
 
     ],
     'params' => $params,
