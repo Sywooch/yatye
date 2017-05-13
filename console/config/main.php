c3fd621fa934d203afeebc78a52ff7bd5142a120
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-05-13 11:35:49 +0200

    Quick fixes

--- console/config/main.php
+++ console/config/main.php
@@ -73,25 +73,6 @@ return [
         'session' => [ // for use session in console application
             'class' => 'yii\web\Session'
         ],
-
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-//            'textLayout' => 'my/layout',  // custome layout
-            'htmlLayout' => 'layouts/html', // disable layout
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',
-                'username' => 'rwandaguide.info@gmail.com',
-                'password' => 'jwcbbofqadxsggnl',
-                'port' => '587',
-                'encryption' => 'tls',
-            ],
-        ],
     ],
     'params' => $params,
 ];
