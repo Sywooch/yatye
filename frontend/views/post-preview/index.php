30a46f2b47b15c756741b2824c3dff9843512182
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-05-14 18:10:50 +0200

    Quick fixes

--- frontend/views/post-preview/index.php
+++ frontend/views/post-preview/index.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="col-sm-4 col-lg-3 p30 visible-md visible-lg">
     <?php if ($model->status != Yii::$app->params['active']) : ?>
-        <?= Html::a(Html::tag('span', '', ['class' => 'fa fa-upload']), ['publish', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary']) ?>
+        <?= Html::a(Html::tag('span', '', ['class' => 'fa fa-upload']), ['/post-preview/publish', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary']) ?>
     <?php endif; ?>
     <?= Html::a(Html::tag('span', '', ['class' => 'fa fa-edit']), ['update', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary']) ?>
     <?php echo Html::a(Html::tag('span', '', ['class' => 'fa fa-remove']), ['delete', 'id' => $model->id], [
