717fde4f361ea7d757d7b4e11812c3c552532f51
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-05-14 11:34:34 +0200

    Quick fixes

--- backend/controllers/EventController.php
+++ backend/controllers/EventController.php
@@ -27,7 +27,7 @@ class EventController extends BaseEventController
         $dataProvider = new ActiveDataProvider([
             'query' => Event::find()
 //                ->where(new Expression('`start_date` >= CURRENT_TIMESTAMP'))
-                ->orderBy(new Expression('`created_at` DESC')),
+                ->orderBy(new Expression('TIMESTAMP(`end_date`,`end_time`) DESC')),
         ]);
 
         return $this->render('index', [
