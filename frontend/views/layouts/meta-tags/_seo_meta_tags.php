a1398dfb6fe02e895e93652fc5f42575f6a2dc41
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-05-19 10:09:16 +0200

    Quick fixes

--- frontend/views/layouts/meta-tags/_seo_meta_tags.php
+++ frontend/views/layouts/meta-tags/_seo_meta_tags.php
@@ -14,14 +14,17 @@ $date = strtotime("+7 day");
 <title><?= Html::encode($this->title) ?></title>
 <meta name="description" content="<?php echo Yii::$app->params['meta_description']; ?>">
 <meta name="keywords" content="<?php echo $keywords; ?>">
-<meta name="robots" content="index, follow"/>
 <meta name="distribution" content="global">
-<meta name="googlebot" content="index, follow"/>
 
 <meta name="revisit-after" content="7 days">
 <meta name="expires" content="<?php echo date('D, d M Y', $date); ?>">
 
+<meta name="robots" content="index, follow"/>
+<meta name="googlebot" content="index, follow"/>
 <meta name="google-site-verification" content="PSEIUTyoAIopuk7Q8t8B6M6WiF711Udi3dT1YE6LBFk"/>
+<meta name="google" content="nositelinkssearchbox" />
+<meta name="google" content="notranslate" />
+
 <meta name="msvalidate.01" content="C8A5F2BA2975673A43AB130C098FB357" />
 <meta name="yandex-verification" content="cce2a42742b85fc7" />
 
