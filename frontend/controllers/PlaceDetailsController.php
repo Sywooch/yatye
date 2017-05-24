14bdb6809ba2f274dfe0c4c1658180ae771bcf25
Marius Ngaboyamahina <marius.ngaboyamahina@wiredin.rw>
2017-05-24 02:18:13 +0200

    Quick fixes

--- frontend/controllers/PlaceDetailsController.php
+++ frontend/controllers/PlaceDetailsController.php
@@ -61,7 +61,7 @@ class PlaceDetailsController extends AdminController
 
 
             $related_place_ids = $model->getRelatedPlaceIds();
-            $related_places = $model->getRelatedPlaces()->all();
+            $related_places = ($model->getRelatedPlaces() != null) ? $model->getRelatedPlaces()->all() : null;
             $other_places = ($model->getNearByPlaces() != null) ? $model->getNearByPlaces()->all() : null;
 
             $nearByPlacesDataProvider = new ActiveDataProvider([
